848aabdd54b38da472f20f71c8fce263734e338a
nahteton <blatch1124211@gmail.com>
2025-03-11 16:55:40 -0700

    Changes to be committed: 	new file:   maps/test.txt 	modified:   model/map.py
    Added map.py functionality

--- model/map.py
+++ model/map.py
@@ -6,22 +6,47 @@ More info later
 """
 
 
-class Terrain:
-    def __init__(self, geo: str = "grassy"):
-        self.geography: str = geo
-
-
 class Tile:
+    def __init__(self, terr: str = "grassy"):
+        self.terrain: str = terr
+
+
+class Region:
     def __init__(self):
         self.rows: int = 100
         self.cols: int = 100
-        self.grid: List[List[Terrain]] = []
+        self.grid: List[List[Tile]] = []
         for _ in range(self.rows):
             self.grid.append([])
 
 
 class Map:
-    def __init__(self, x: int = 1, y: int = 1):
-        self.grid: List[List[Tile]] = []
+    def __init__(self, x: int = 1, y: int = 1, z: int = 1):
+        self.grid: List[List[Region]] = []
         self.rows: int = x
         self.cols: int = y
+        self.regionSize: int = z
+
+    def importMap(self):
+        val = input("Enter map path: ")
+        # path = ..\\maps\\test.txt
+
+        with open(path, "r") as file:
+            self.rows = file.readline()
+            self.cols = file.readline()
+            self.regionSize = file.readline()        
+            lines = file.readlines()
+
+            for line in lines:
+                for char in line:
+                    print(char, end = " ")
+                print()
+        return
+
+
+
+def main():
+    Map().importMap()
+
+if __name__ == "__main__":
+    main()

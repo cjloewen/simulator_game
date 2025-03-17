aaf02154b783783a8d981596e45d6b629c079073
Cameron <cameronjloewen@outlook.com>
2025-03-17 11:44:02 -0700

    more buffers

--- model/map.py
+++ model/map.py
@@ -4,6 +4,7 @@ import math
 """
 Class: Map
 More info later
+Grid XY is actually rows columns, not cartesian (x,y), eventually can resolve this, maybe with the map buffer?
 """
 
 
@@ -46,6 +47,8 @@ class Region:
         for _ in range(self.rows):
             self.grid.append([])
 
+    def getTile(self, x: int, y: int):
+        return self.grid[y][x]
 
 class Map:
     def __init__(self, x: int = 1, y: int = 1, z: int = 1):
@@ -56,7 +59,13 @@ class Map:
 
         self.importMap()
 
+    def getTile(self, x: float, y: float):
+        x1,x2,y1,y2 = self.convertToIndices(x, y)
+        return self.grid[y1][x1].getTile(x2, y2)
+
+
     def convertToIndices(self, x: float, y: float):
+        size = self.regionSize
         globalX = math.floor(x + .5)
         globalY: int = math.floor( y + .5)
         blockSize = self.regionSize
@@ -64,6 +73,13 @@ class Map:
         block_y = math.floor(globalY / blockSize)
         tile_x = globalX % blockSize
         tile_y = globalY % blockSize
+
+        # convert global coordinates to actual array indices, index(0,0) is the top left of the map
+        # also, x corresponds to the inner index, so it is indexed like array[y][x]
+        block_x += self.rows // 2
+        block_y = self.cols // 2 - block_y
+        tile_x += size // 2 + tile_y
+        tile_y = size // 2 - tile_y
 
         return (block_x, block_y, tile_x, tile_y)
     
--- model/mapBuffer.py
+++ model/mapBuffer.py
@@ -1,28 +1,47 @@
 from model.map import Map, Region, Tile
-
+from model.character import Character
+from typing import List, Tuple, Dict
+import math
 
 '''
 Essentially a buffer of what part of the map is actually "loaded"
-For now, this is just for displaying the map
+For now, this is just for displaying the map (I think it should just be the whole buffer?)
 Later, it should either also update everything within it, or have a sister class that deals with that
+IMPORTANT: MAP BUFFER SHOULD NOT BE ALLOWED TO CHANGE THE MAP/REGION/TILE DATA
 '''
+
+class Buffer:
+    def __init__(self):
+            self.centerX: int = 0
+            self.centerY: int = 0
+
+class unitBuffer:
+    def __init__(self):
+        self.characters: List[Character] = []
+        self.positions: Dict[Character, Tuple[int, int]] = {}
+
 class MapView:
     def __init__(self, map: Map, size: int):
-        self.map = Map
+        self.map = map
         self.size = size
-        self.centerX: float = 0
-        self.centerY: float = 0
+        self.grid: List[List[Tile]] = []
+        for _ in range(self.size):
+            self.grid.append([])
 
-    def setCenter(self, x: float, y: float):
-        self.centerX = x
-        self.centerY = y
 
     '''
     If the map itself changes (for whatever reason)
     Or when the center moves too far away, and we get close to the edge of memory
     '''
-    def regenerateBuffer(self):
-        pass
+    def regenerateBuffer(self, centerX: int, centerY: int):
+        map = self.map
+        size = self.size
+        counterY = 0
+        for x in range(centerX - math.floor(size / 2), centerX + math.ceil(size / 2)):
+            for y in range(centerY + math.ceil(size / 2), centerY - math.floor(size / 2), -1):
+                curTile = map.getTile(x, y)
+                self.grid[counterY].append(curTile)
+            counterY += 1
 
     def getStringRep(self):
         pass
